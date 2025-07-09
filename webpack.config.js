cf819a8e7b267d97823f554568fb24e13b832161
Maxim Predybailo <predmaxim@ya.ru>
2025-07-09 23:00:08 +0500

    feat: update build scripts for improved directory handling

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
-
+console.log('NODE_ENV:', process.env.NODE_ENV);
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'screen-reader.bundle.js',
-    path: path.resolve(__dirname, 'test_page'),
+    filename: 'screen-reader.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
   },
   module: {
     rules: [
@@ -14,8 +15,8 @@ module.exports = {
       },
     ],
   },
-  mode: 'development',
-  devtool: 'source-map',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
   devServer: {
     static: {
       directory: path.join(__dirname, 'test_page'),
